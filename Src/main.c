e89a77a6fab72d8f453b3784af6d270438fdfd4e
Albes K <koxhaj19@hotmail.com>
2019-03-16 14:19:26 +0000

    Working on keeping unique data for each node, Updated insert function to do just that albeit not being as perfect as the top down but more costly in time approach as it includes pseudobodies as it goes down the tree. Hopefully a sum function upwards might fix that for the bottom up approach as first referenced by Barnes-Hut in their first paper.

--- Src/main.c
+++ Src/main.c
@@ -79,7 +79,7 @@ struct quad* newNode(int data, double s, double x, double y)
 
 	
 /*
-    Recursively display tree or subtree
+    Recursively display tree or subtree inorder
 */
 void display_tree(struct quad* nd)
 {
@@ -107,7 +107,7 @@ void display_tree(struct quad* nd)
 }
 
 /*
-    Deconstruct quad tree
+    Deconstruct quad tree (postorder)
 */ 
 void deconstruct_tree(struct quad* root)
 {
@@ -125,22 +125,36 @@ void deconstruct_tree(struct quad* root)
 /*
     Subdivide node to 4 quadrants and assign memory dynamically through newNode() function
 */
-void subdivide(struct quad* nd){ 
+void subdivide(struct quad* nd, int* track){ 
     
     if(nd == NULL){ // If there is no node do not subdive. (safety measure)
         return;
     }
-    
-    printf("Subdivide call at: %i \n", nd->data);
-    
+    // printf("Subdivide call at: %i \n", nd->data);
     // Call newNode function for each child node that was Null of the node at hand and assign a memory block of size (struct quad)
-    // -1 is assigned here if the node is a 'twig' meaning it is not a 'leaf' for now empty cells are also -1.
-    nd->NE = newNode(-1, nd->s/2, nd->centre.x + nd->s/4, nd->centre.y + nd->s/4);
-    nd->SE = newNode(-1, nd->s/2, nd->centre.x + nd->s/4, nd->centre.y - nd->s/4);
-    nd->SW = newNode(-1, nd->s/2, nd->centre.x - nd->s/4, nd->centre.y - nd->s/4);
-    nd->NW = newNode(-1, nd->s/2, nd->centre.x - nd->s/4, nd->centre.y + nd->s/4); 
+    // -n is assigned here if the node is a 'twig' meaning it is not a 'leaf' for now empty cells are also -n where n integers that
+    // that are unique for each quad.
+    //    _____________________
+    //   |          |          |
+    //   |   (NW)   |   (NE)   |
+    //   |          |          |
+    //   |    -+    |    ++    |
+    //   |__________|__________|
+    //   |          |          |
+    //   |    --    |    +-    |
+    //   |          |          |
+    //   |   (SW)   |   (SE)   |
+    //   |__________|__________|
 
+    nd->NE = newNode(*track-1, nd->s/2, nd->centre.x + nd->s/4, nd->centre.y + nd->s/4); 
+    nd->SE = newNode(*track-2, nd->s/2, nd->centre.x + nd->s/4, nd->centre.y - nd->s/4); 
+    nd->SW = newNode(*track-3, nd->s/2, nd->centre.x - nd->s/4, nd->centre.y - nd->s/4); 
+    nd->NW = newNode(*track-4, nd->s/2, nd->centre.x - nd->s/4, nd->centre.y + nd->s/4); 
+    
+      
     nd->divided = true; // The node subdivided ( safety for not subdividing again the same node )
+    *track = *track-4;
+    // printf("Track is: %i\n", *track);
 
 }
 
@@ -152,7 +166,7 @@ bool contains(struct quad* nd, struct point p){
         return (p.x < (nd->centre.x+nd->s/2) &&
         p.x > (nd->centre.x-nd->s/2) &&
         p.y < (nd->centre.y+nd->s/2) &&
-        p.y > (nd->centre.x-nd->s/2));
+        p.y > (nd->centre.y-nd->s/2));
         
 }
 
@@ -160,7 +174,7 @@ bool contains(struct quad* nd, struct point p){
 /*
     Construct quad tree from bottom-up by putting bodies inside it.
 */
-int insert(struct quad* nd, struct body* b, int *index){
+int insert(struct quad* nd, struct body* b, int *index, int* track){
 
     // If current quad cannot contain it 
     if (!contains(nd,b->pos)){ 
@@ -170,30 +184,32 @@ int insert(struct quad* nd, struct body* b, int *index){
     if(nd->b==NULL){ // If there is no pointer to body assign it (Essentially capacity is kept at 1 here with this method)
         nd->b = b;
         nd->data = *index; //Assign the number of the body from the Bodies array, this is for getting back with data where the body is stored as a leaf
-        printf("Pointer to %i\n", nd->data);
-        return 1; // Found so return 1 so we can exit the recursion
+        // printf("Pointer to %i\n", nd->data);
+        return 0; // Found so return 0 so we can exit the recursion
     } 
     else{
     
         if(nd->divided!=true){ // Check if the quad quad has subdivided
-            subdivide(nd); // If not, subdivide!
+            subdivide(nd, track); // If not, subdivide!
+            // Check where to put nodes body and then after the if statement check for the current index body
+            int temp = nd->data;
+            nd->data = *track+4;
+            *track = *track-1;
+            insert(nd->NE, nd->b, &temp, track);  
+            insert(nd->SE, nd->b, &temp, track);  
+            insert(nd->SW, nd->b, &temp, track); 
+            insert(nd->NW, nd->b, &temp, track);
         }
-            
+    }
 
-        return insert(nd->NE, b, index)|| // Since insert is an int function, the return statement here returns 1 or 0 if 
-            insert(nd->SE, b, index)||  // a node is found to point the body and thus save the body at. So for example if we
-            insert(nd->SW, b, index)|| // have 2 bodies and the first one ( zeroth ) is always saved at root ( being empty and not divided )
-            insert(nd->NW, b, index); // then the second Body 1 if it is at NW subcell after division when it goes to the return state-
+    return insert(nd->NE, b, index, track)|| // Since insert is an int function, the return statement here returns 1 or 0 if 
+    insert(nd->SE, b, index, track)||  // a node is found to point the body and thus save the body at. So for example if we
+    insert(nd->SW, b, index, track)|| // have 2 bodies and the first one is always saved at root ( being empthy and not divided)
+    insert(nd->NW, b, index, track); // then the second Body 1 if it is at NW subcell after division when it goes to the return state-
         // -ment it will go through the OR terms as the contain function will not let the first 3 OR terms ( being insert(nd->NE,...) || ... 
         // to insert(nd->SW,...) and pick the insert(nd->NW,...) to assign. The same process is repeated every time the function calls itself
         // and checks for where to put the body.
-        
-        
-    }
-
-
 }
-
 
 /*
     Search quad tree for node with specific data in inorder format
@@ -210,6 +226,138 @@ struct quad* Search(struct quad* root, int data) {
 	Search(root->SW, data);    // Visit SW subtree
     Search(root->SE, data);   // Visit SE subtree
     Search(root->NE, data);  // Visit NE subtree
+}
+
+void xyt_data_particles(struct body* bodies, int* N_PARTICLES, double t){
+    FILE * f; 
+    f = fopen("/home/albes/Desktop/bodiestd.txt", "w"); /* open the file for writing*/
+    printf("Writting...\n");
+    /* write 10 lines of text into the file stream*/    
+    fprintf(f, "N,X,Y,M,C,T\n");
+
+    for(int i = 0; i < *N_PARTICLES;i++){
+        fprintf (f, "%d,%f,%f,%f,%f\n", i, bodies[i].pos.x, bodies[i].pos.y, bodies[i].mass, bodies[i].charge);
+    }
+    fprintf(f,",,,,,%f",t);
+
+    /* close the file*/  
+    fclose (f);
+    printf("Closed file.\n");
+}
+
+/*
+    Helper function for xy_tree.c to print data to csv file so it can be processed later
+*/
+void printdata(struct quad* nd, FILE* f){
+    if (nd == NULL){return;}
+    // Display the data of the node
+    fprintf(f,"%d,%f,%f,%f\n",nd->data, nd->centre.x, nd->centre.y, nd->s);
+    
+    // Recurse through
+    printdata(nd->NE,f);    
+    printdata(nd->SE,f);
+    printdata(nd->SW,f);
+    printdata(nd->NW,f);
+}
+
+/*
+    Function to print quad tree data into csv files
+*/
+void xy_trees(struct quad* nd){
+    FILE * f; 
+    f = fopen("/home/albes/Desktop/nodestd.txt", "w"); /* open the file for writing*/
+    printf("Writting...\n");
+    /* write 10 lines of text into the file stream*/    
+    fprintf(f, "N,X,Y,S\n");
+    printdata(nd, f);
+    /* close the file*/  
+    fclose (f);
+    printf("Closed file.\n");
+}
+
+struct linkedList
+{
+  struct quad* data;
+  struct linkedList* next;
+
+};
+
+struct linkedList* begin;
+struct linkedList* end;
+
+/*
+    Boolean function checking if queue is empty
+*/
+bool queue_empty()
+{
+  return (begin==NULL && end==NULL);
+}
+
+/*
+    Push element inside the end part of the queue by allocating new memory dynamically
+*/ 
+void enqueue(struct quad* nd)
+{
+    struct linkedList* temp = malloc(sizeof(struct linkedList));
+    temp->data = nd;
+    temp->next = NULL;
+    if(begin==NULL && end==NULL){
+        begin = end = temp;
+    }
+    end->next = temp;
+    end = temp;
+}
+
+ /*
+    Pop element from queue and free its memory
+ */
+void dequeue()
+{
+    struct linkedList* temp = begin;
+    if(begin==NULL){return;}
+    if(begin==end){
+        begin = NULL;
+        end = NULL;
+    }
+    else{
+        begin = begin->next;                    
+    }
+    free(temp);
+}
+
+/*
+    Level Order Traversal for force summation ( Breadth first traversal)
+*/
+void levelorder(struct quad* n)
+{
+    enqueue(n);
+    enqueue(NULL); // Extra NUll parameter for checking when the tree goes to the next level after enquing all children in one level.
+    struct quad* curr = NULL;
+    while (!queue_empty())
+    {
+    
+    curr = begin->data;
+    dequeue();
+    if(curr!=NULL){
+        
+        if (curr->NE)
+            enqueue(curr->NE);
+        if (curr->SE)
+            enqueue(curr->SE);
+        if (curr->SW)
+            enqueue(curr->SW);
+        if (curr->NW)
+            enqueue(curr->NW);
+        printf("%d ",curr->data);
+    }
+    else{
+        printf("\n");
+        if(!queue_empty()){
+            enqueue(NULL);
+        }
+    }
+  
+    }
 }
 
 /*
@@ -248,7 +396,7 @@ int main() {
             struct body b = {.mass = mass, .charge = charge, .pos = p };
 
             bodies[i] = b;
-            printf("%c:[%f], %c:[%f] \n", x[0], bodies[i].pos.x, x[1], bodies[i].pos.y );
+            // printf("%c:[%f], %c:[%f] \n", x[0], bodies[i].pos.x, x[1], bodies[i].pos.y );
     
     }
 
@@ -257,17 +405,31 @@ int main() {
 
     struct quad *root = newNode(0, 100, 0, 0); //Size of s=100 and pint of reference being (0,0) equiv. to (x_root, y_root)  
     //printf("Root square size is: %f\n", root->s);
-    
+    int track = 0;
     ts = clock(); // Start timer
     for(int i=0; i<N_PARTICLES; i++){
-        insert(root, &bodies[i], &i);
+        insert(root, &bodies[i], &i, &track);
     }
     te = clock(); // End timer
     double d = (double)(te-ts)/CLOCKS_PER_SEC; // Bottom-up tree construction time
     
-    display_tree(root);
+    // display_tree(root);
+    char c;
+    printf("Do you want to save the data? Y/n \n");
+    scanf("%c", &c);
+    
+    if(c=='Y'){
+        printf("Saving bodies data...\n");
+        xyt_data_particles(bodies,&N_PARTICLES, d);
+        printf("Saving tree data...\n");
+        xy_trees(root);
+        printf("Done\n");
+    } else
+    {
+        printf("Continuing\n");   
+    }
 
-    // deconstruct the tree
+    levelorder(root);
     deconstruct_tree(root);
     free(bodies);
 
